e27832c8646a9d9bb4d8cc6bda169cab0ee6ce9a
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-08-17 11:55:29 -0300

    feat: criando o base controller

---
+++ GestaoDeLivraria/Controllers/GestaoDeLivrariaBaseController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeLivraria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestaoDeLivrariaBaseController : ControllerBase
+    {
+    }
+}
